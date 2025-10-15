c5e22f816e906fffdc7cc8034747bde1f34f9d6f
marijaT13 <tashevska254@gmail.com>
2025-10-15 13:43:56 +0200

    used gltf to jsx converter to make 3D models and added one model

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -2,6 +2,7 @@ import clsx from "clsx";
 import useMacBookStore from "../store";
 import { Canvas } from "@react-three/fiber";
 import { Box, OrbitControls } from "@react-three/drei";
+import MacbookModel14 from "./models/Macbook-14";
 
 const ProductViewer = ()=>{
     const{ color, scale, setColor, setScale} = useMacBookStore();
@@ -37,11 +38,9 @@ const ProductViewer = ()=>{
             </div>
 
             <Canvas id="canvas" camera={{position:[0, 2, 5], fov:50, near:0.1, far:100}}> {/*field of view(fov) */}
-                <Box 
-                position={[0 , 0 , 0]} 
-                scale={10 * scale} 
-                material-color={color}
-                />
+                <ambientLight intensity={0.8} />
+                <MacbookModel14 scale={0.06} position={[-1, 0, 0]}/>
+                
                 <OrbitControls 
                 enableZoom={false} 
                 />
