f498dd8c09ee94de24168b3ad9ed0ae06eba032f
marijaT13 <tashevska254@gmail.com>
2025-10-17 14:23:21 +0200

    Implemented highlights section and it's animations

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,7 +1,67 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useMediaQuery } from "react-responsive";
 
 const Highlights =()=>{
+    const isMobile = useMediaQuery({query: '(max-width: 1024px)'});
+    
+    useGSAP(()=>{
+        gsap.to(['.left-column', '.right-column'],{
+            scrollTrigger:{
+                trigger:'#highlights',
+                start: isMobile ? 'bottom bottom' : 'top top'
+            },
+            y:0,
+            opacity:1,
+            stagger: 0.5,
+            duration:1,
+            ease:'power1.inOut' 
+        });
+    })
+
     return(
-        <div>Highlighted Components</div>
+        <section id="highlights">
+            <h2>There&apos;s never been a better time to upgrade.</h2>
+            <h3>Here&apos;s what you get with the new MacBook Pro.</h3>
+            <div className="masonry">
+
+                <div className="left-column">
+                    <div>
+                        <img src="/laptop.png" alt="laptop" />
+                        <p>Fly through demanding tasks up to 9.8x faster.</p>
+                    </div>
+                    <div>
+                        <img src="/sun.png" alt="sun" />
+                        <p>A stunning<br/>
+                           Liquid Retina XDR <br/>
+                           display with extreme <br/>
+                           dynamic range and <br/>
+                           contrast ratio.
+                        </p>
+                    </div>
+                </div>
+
+                <div className="right-column">
+                    <div className="apple-gradient">
+                        <img src="/ai.png" alt="ai" />
+                        <p>Build for <br/>
+                            <span>
+                            Apple Intelligence.
+                            </span>
+                        </p>
+                    </div>
+                    <div>
+                        <img src="/battery.png" alt="battery" />
+                        <p>Up to 
+                            <span  className="green-gradient">14 more <br/> hours </span> 
+                            battery life.<br/>
+                           <span className="text-dark-100">(Up to 24 hours<br/>
+                            total.)</span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
